547d8753d557d139260d21409804c3a5d787a8a8
Milywayz <jtjenks@gmail.com>
2023-05-06 11:59:27 -0500

    added switch case for list start function

--- index.js
+++ index.js
@@ -5,31 +5,72 @@ const pool = mysql.createPool({
     host: 'localhost',
     user: 'root',
     password: "rootroot",
-    database: '',
+    database: 'employment_db',
     waitForConnections: true,
     connectionLimit: 10,
     maxIdle: 10,
     idleTimeout: 6000,
     queueLimit: 0
 })
-
-
 const promisePool = pool.promise();
+
+async function start() {
+    const initialPrompt = await inquirer
+        .prompt([
+            {
+                type: 'list',
+                name: 'shape',
+                message: '',
+                choices: ['View All Departments', 'View All Roles', 'View All Employees', 'Add A Department', "Add A Role", "Add A Employee", 'Add a Manager', 'Update An Employee Role'],
+
+            },
+        ])
+}
+
+switch (initialPrompt) {
+
+    case 'View All Departments':
+        viewAllDepartments();
+        break;
+        case 'View All Roles':
+        ViewAllRoles();
+        break;
+        case 'View All Employees':
+        viewAllEmployees();
+        break;
+        case 'Add A Department':
+        AddADepartment();
+        break;
+        case 'Add A Role':
+        AddARole();
+        break;
+        case 'Add A Employee':
+        AddAEmployee();
+        break;
+        case 'Add a Manager':
+        AddAManager();
+        break;
+        case 'Update An Employee Role':
+        UpdateAnEmployeeRole();
+        break;
+    default:
+        break;
+}
 
 findEmployee();
 
-async function findEmployee(){
+async function findEmployee() {
 
 
     let { } = await inquirer.prompt({
-        
-    
+
+
     })
-    
-    
-            const [rows,fields] = await promisePool.query("SELECT * FROM ")
-    
-            console.table(rows)
+
+
+    const [rows, fields] = await promisePool.query("SELECT * FROM ")
+
+    console.table(rows)
 
 
 }
