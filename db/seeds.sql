3b65d53f508640465d0ae8b7e5fb1eb54466bd8e
Milywayz <jtjenks@gmail.com>
2023-05-06 19:37:46 -0500

    fixed seeds and manager bug

--- db/seeds.sql
+++ db/seeds.sql
@@ -10,41 +10,30 @@ VALUES
 INSERT INTO roles (title, salary, department_id)
 VALUES
 ('Chief marketing Officer', 65000.00, 1),
+('Marketing Consultant', 55000.00, 1),
 ('Operations Executive', 85000.00, 2),
-('Accounts Payable Head', 60000.00, 3),
+('Operations', 57000.00, 2),
+('Accounts Payable Head', 80000.00, 3),
+('Accountant', 49000.00, 3),
 ('Sales Executive Manager', 100000.00, 4),
+('SalesMan', 75000.00, 4),
 ('Chief Human Capital Officer', 90000.00, 5),
-('Merchandising Manager', 75000.00, 6);
+('Human Resource Development', 68000.00, 5),
+('Merchandising Manager', 75000.00, 6),
+('Merchandising', 50000.00, 6);
 
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
 VALUES
-('Glee', 'Anselm', 1, 1),
-('Thea', ' Johansen', 2, 2),
-('Clara', 'Hunt', 3, 3),
-('Summer', 'Evans', 4, 4),
-('Ronald', 'Rice', 5, 5),
-('Nadislava', 'Pavlovskiy', 6, 6);
+('Joshua', 'Thomas', 1, NULL),
+('Freja', 'Hansen' ,2, 1),
+('Angel', 'Pena', 3, NULL),
+('Birgit', 'Mathieu', 4, 2),
+('Ronja', 'Rautio', 5, NULL),
+('Lau', 'Alon', 6, 3),
+('Nastasia', 'Kadri', 7, NULL),
+('Adelma', 'Hanzou', 8, 4),
+('Othello', 'Yvon', 9, NULL),
+('Adriel', 'Ilie', 10, 5),
+('Max', 'Rowland', 11, NULL),
+('Fife', 'Teodor', 12, 6);
 
--- INSERT INTO roles (title, salary, department_id)
--- VALUES
--- ('Chief marketing Officer', 65000.00, 1),
--- ('Marketing Consultant', 55000.00,1),
--- ('Operations Executive', 85000.00, 2),
--- ('Operations', 57000.00, 2),
--- ('Accounts Payable Head', 80000.00, 3),
--- ('Accountant', 49000.00, 3),
--- ('Sales Executive Manager', 100000.00, 4),
--- ('SalesMan', 75000.00, 4),
--- ('Chief Human Capital Officer', 90000.00, 5),
--- ('Human Resource Development', 68000.00, 5),
--- ('Merchandising Manager', 75000.00, 6),
--- ('Merchandising', 50000.00, 6);
-
--- INSERT INTO employee (first_name, last_name, role_id, manager_id)
--- VALUES
--- ('Joshua', 'Thomas', 7),
--- ('Freja', 'Hansen' ,8),
--- ('Angel', 'Pena', 9),
--- ('Birgit', 'Mathieu', 10),
--- ('Ronja', 'Rautio', 11),
--- ('Lau', 'Alon', 12);
--- index.mjs
+++ index.mjs
@@ -86,175 +86,188 @@ async function ViewAllRoles() {
 }
 
 async function viewAllEmployees() {
-    
-    const [rows] = await promisePool.query(`SELECT e.id, e.first_name, e.last_name, r.title, d.department_name, r.salary, CONCAT(e.first_name, ' ', e.last_name) AS manager_name
-    FROM employee AS e
-    JOIN roles AS r ON e.role_id = r.id
-    JOIN departments AS d ON r.department_id = d.id;`)
-    
+
+    const [rows] = await promisePool.query(`SELECT e.id AS employee_id, e.first_name AS employee_first_name, e.last_name AS employee_last_name,
+    r.title, d.department_name, r.salary,
+    CONCAT(m.first_name, ' ', m.last_name) AS manager
+FROM employee e
+LEFT JOIN employee m ON e.manager_id = m.id
+LEFT JOIN roles r ON e.role_id = r.id
+LEFT JOIN departments d ON r.department_id = d.id;`)
+
     if (rows.length === 0) {
         console.log("No employee to display.");
     } else {
         console.table(rows)
         start()
     }
-    
+
 }
 
 async function AddADepartment() {
 
     let { departmentName } = await inquirer.prompt([
-     {
-         type: 'input',
-         name: 'departmentName',
-         message: 'What is the name of the department?',
-     }
+        {
+            type: 'input',
+            name: 'departmentName',
+            message: 'What is the name of the department?',
+        }
     ])
-    
-    const [rows] = await promisePool.query('INSERT INTO departments (department_name) VALUES (?)', departmentName, function (err, results) {
-     if (rows.length === 0) {
-         console.log("Can't enter in that department");
-     } else {
-         console.table(rows)
-         start()
-     }
- 
-    })
- 
 
- }
+    const [rows] = await promisePool.query('INSERT INTO departments (department_name) VALUES (?)', departmentName, function (err, results) {
+        if (rows.length === 0) {
+            console.log("Can't enter in that department");
+        } else {
+            console.table(rows)
+            start()
+        }
+
+    })
+
+
+}
 async function AddARole() {
     const [rowS] = await promisePool.query("SELECT department_name FROM departments")
     const departments = rowS.map(row => row.department_name);
-    
-    let { title, salary, departmentId} = await inquirer.prompt([
+
+    let { title, salary, departmentId } = await inquirer.prompt([
         {
-        type: 'input',
-        name: 'title',
-        message: 'What is the title of the role?',
-    },
-    {
-        type: 'input',
-        name: 'salary',
-        message: 'What is the salary of the role?',
-    },
-    {
-        type: 'list',
-        name: 'departmentId',
-        message: 'What is the departmentId of the role? (please use an existing departmentID)',
-        choices: [...departments]
-    }
-   ])
+            type: 'input',
+            name: 'title',
+            message: 'What is the title of the role?',
+        },
+        {
+            type: 'input',
+            name: 'salary',
+            message: 'What is the salary of the role?',
+        },
+        {
+            type: 'list',
+            name: 'departmentId',
+            message: 'What is the departmentId of the role?',
+            choices: [...departments]
+        }
+    ])
 
-   const [rows] = await promisePool.query('INSERT INTO roles (title, salary, department_id) VALUES (?, ?, ?)', [title, salary, departmentId], function (err, results) {
-    if (rows.length === 0) {
-        console.log("Please make sure you put in an departmentId that already exist");
-    } else {
-        console.table(rows)
-        start()
-    }
+    const [rows] = await promisePool.query('INSERT INTO roles (title, salary, department_id) VALUES (?, ?, ?)', [title, salary, departmentId], function (err, results) {
+        if (rows.length === 0) {
+            console.log("Please make sure you put in an departmentId that already exist");
+        } else {
+            console.table(rows)
+            start()
+        }
 
-   })
+    })
 
 }
 
 async function AddAEmployee() {
-    
-    let { firstName, lastName, roleId} = await inquirer.prompt([
+    const [rowS] = await promisePool.query("SELECT role_id FROM employee")
+    const employees = rowS.map(row => row.role_id);
+
+    let { firstName, lastName, roleId } = await inquirer.prompt([
         {
-        type: 'input',
-        name: 'firstName',
-        message: 'What is the first name of the employee?',
-    },
-    {
-        type: 'input',
-        name: 'lastName',
-        message: 'What is the last name of the employee?',
-    },
-    {
-        type: 'input',
-        name: 'roleId',
-        message: 'What is the role ID of the employee? (please use an existing roleID)',
-    }
-   ])
+            type: 'input',
+            name: 'firstName',
+            message: 'What is the first name of the employee?',
+        },
+        {
+            type: 'input',
+            name: 'lastName',
+            message: 'What is the last name of the employee?',
+        },
+        {
+            type: 'list',
+            name: 'roleId',
+            message: 'What is the role ID of the employee?',
+            choices: [...employees]
+        }
+    ])
 
-   const [rows] = await promisePool.query('INSERT INTO employee (first_name, last_name, role_id) VALUES (?, ?, ?)', [firstName, lastName, roleId], function (err, results) {
-    if (rows.length === 0) {
-        console.log("Can't enter in that employee");
-    } else {
-        console.table(rows)
-        start()
-    }
+    const [rows] = await promisePool.query('INSERT INTO employee (first_name, last_name, role_id) VALUES (?, ?, ?)', [firstName, lastName, roleId], function (err, results) {
+        if (rows.length === 0) {
+            console.log("Can't enter in that employee");
+        } else {
+            console.table(rows)
+            start()
+        }
 
-   })
+    })
 
 }
 
 async function AddAManager() {
-    
-    let { firstName, lastName, roleId, managerId} = await inquirer.prompt([
+    const [rowS] = await promisePool.query("SELECT role_id FROM employee")
+    const employees = rowS.map(row => row.role_id);
+
+    const [rowSs] = await promisePool.query("SELECT manager_id FROM employee")
+    const managers = rowSs.map(row => row.manager_id);
+
+    let { firstName, lastName, roleId, managerId } = await inquirer.prompt([
         {
-        type: 'input',
-        name: 'firstName',
-        message: 'What is the first name of the employee?',
-    },
-    {
-        type: 'input',
-        name: 'lastName',
-        message: 'What is the last name of the employee?',
-    },
-    {
-        type: 'input',
-        name: 'roleId',
-        message: 'What is the role ID of the employee? (please use an existing roleID)',
-    },
-    {
-        type: 'input',
-        name: 'managerId',
-        message: 'What is the manager ID of the employee?',
-    }
-   ])
+            type: 'input',
+            name: 'firstName',
+            message: 'What is the first name of the employee?',
+        },
+        {
+            type: 'input',
+            name: 'lastName',
+            message: 'What is the last name of the employee?',
+        },
+         {
+            type: 'list',
+            name: 'roleId',
+            message: 'What is the role ID of the employee?',
+            choices: [...employees]
+        },
+        {
+            type: 'list',
+            name: 'managerId',
+            message: 'What is the role ID of the employee?',
+            choices: [...managers]
+        }
+    ])
 
-   const [rows] = await promisePool.query('INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES (?, ?, ?, ?)', [firstName, lastName, roleId, managerId], function (err, results) {
-    if (rows.length === 0) {
-        console.log("Can't enter in that manager employee");
-    } else {
-        console.table(rows)
-        start()
-    }
+    const [rows] = await promisePool.query('INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES (?, ?, ?, ?)', [firstName, lastName, roleId, managerId], function (err, results) {
+        if (rows.length === 0) {
+            console.log("Can't enter in that manager employee");
+        } else {
+            console.table(rows)
+            start()
+        }
 
-   })
+    })
 
 }
 
 async function UpdateAnEmployeeRole() {
-    
-    let { firstName, lastName, roleId} = await inquirer.prompt([
+
+    let { firstName, lastName, roleId } = await inquirer.prompt([
         {
-        type: 'input',
-        name: 'firstName',
-        message: 'What is the first name of the employee?',
-    },
-    {
-        type: 'input',
-        name: 'lastName',
-        message: 'What is the last name of the employee?',
-    },
-    {
-        type: 'input',
-        name: 'roleId',
-        message: 'What is the role ID of the employee? (please use an existing roleID)',
-    }
-   ])
+            type: 'input',
+            name: 'firstName',
+            message: 'What is the first name of the employee?',
+        },
+        {
+            type: 'input',
+            name: 'lastName',
+            message: 'What is the last name of the employee?',
+        },
+        {
+            type: 'input',
+            name: 'roleId',
+            message: 'What is the role ID of the employee? (please use an existing roleID)',
+        }
+    ])
 
-   const [rows] = await promisePool.query('UPDATE employee SET  (first_name, last_name, role_id) VALUES (?, ?, ?)', [firstName, lastName, roleId], function (err, results) {
-    if (rows.length === 0) {
-        console.log("Can't enter in that employee");
-    } else {
-        console.table(rows)
-        start()
-    }
+    const [rows] = await promisePool.query('UPDATE employee SET  (first_name, last_name, role_id) VALUES (?, ?, ?)', [firstName, lastName, roleId], function (err, results) {
+        if (rows.length === 0) {
+            console.log("Can't enter in that employee");
+        } else {
+            console.table(rows)
+            start()
+        }
 
-   })
+    })
 
 }
